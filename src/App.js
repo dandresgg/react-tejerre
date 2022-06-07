7e698c606686510f8d9655495a08484119c620c5
diego <diegogomez268@gmail.com>
2022-06-07 12:21:17 -0500

    app css refactored

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import './css/btn.css';
+import React from 'react';
+import './css/img.css'
+import {
+    Routes,
+    Route,
+} from "react-router-dom";
+import Replacements from './views/replacements';
+import Auth from './views/auth';
+import ProfileDetails from './views/profile-detail';
+import Contact from './views/contact';
+import Cart from './views/cart';
+import {MenuOptions} from './parts/menu';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header mayus">
+                <h3>TejeRepuestos</h3>
+            </header>
+            <div className="d-flex space-b">
+                <div className='container-left bg-main'>
+                    <MenuOptions />
+                </div>
+                <div className='container center'>
+                    <Routes>
+                        <Route path='/perfil' caseSensitive={false} element={<Auth />} />
+                        <Route path='/repuestos' caseSensitive={false} element={<Replacements />} />
+                        <Route path='/contacto' caseSensitive={false} element={<Contact />} />
+                        <Route path='/carro' caseSensitive={false} element={<Cart />} />
+                        <Route path='/perfil/detalles' caseSensitive={false} element={<ProfileDetails />} />
+                    </Routes>
+                </div>
+            </div>
+            <div className='App-footer'>
+                Copyright © 2022 TejeRepuestos
+            </div>
+        </div>
+    );
 }
-
-export default App;
