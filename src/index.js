dd97c8f1c8384d0cc7c420b1fdecf37e6a16b064
diego <diegogomez268@gmail.com>
2022-06-07 12:21:37 -0500

    index adding routes and cookies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App} from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+    BrowserRouter as Router,
+} from "react-router-dom";
+import { CookiesProvider} from 'react-cookie';
+
+function RouterManinPage() {
+    return (
+        <CookiesProvider>
+            <Router>
+                <App />
+            </Router>
+        </CookiesProvider>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterManinPage />
 );
 
 // If you want to start measuring performance in your app, pass a function
