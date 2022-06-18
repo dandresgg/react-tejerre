81385f06052104b9fab5f5ef4bbfbade58a38515
diego <diegogomez268@gmail.com>
2022-06-18 09:14:26 -0500

    cart view useefect to change is user is logged

--- src/views/cart.js
+++ src/views/cart.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../css/cart.css';
 import {useNavigate} from 'react-router-dom';
 import {ButtonSign, Anchor} from '../css/btn.js';
+import {useCookies} from 'react-cookie';
 
 function Cart() {
+    const [token, setToken] = useCookies(['token'])
+    useEffect(() => {
+        if (token['token']) {
+            window.location.href = '/cart/detalles/'
+        }
+    }, [token])
     let navigate = useNavigate();
     const goRegister = () => {
         let path = '/perfil';
@@ -11,8 +18,9 @@ function Cart() {
     }
     return (
         <div>
-            <div className='mt-menu cart-board center'>
-                <h1 className='w-50 center'>Para agregar al carrito, ingresa a tu cuenta</h1>
+            <div className='mt-menu center'>
+                <h1>Carrito de compra</h1>
+                <h4 className='w-50 center gray'>Para agregar al carrito, ingresa a tu cuenta</h4>
                 <div className='mt-cart'>
                     <ButtonSign onClick={goRegister}>Crear Cuenta</ButtonSign>
                 </div>
