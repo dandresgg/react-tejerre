e7d156dd5dc5185d0d5d6777d9ee402f3fc79763
diego <diegogomez268@gmail.com>
2022-06-18 09:17:11 -0500

    new route cart detail, new cookie cartitems new props set cartitems to cart detail

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './css/btn.css';
-import React from 'react';
+import React, {useState} from 'react';
 import './css/img.css'
 import {
     Routes,
@@ -11,30 +11,57 @@ import Auth from './views/auth';
 import ProfileDetails from './views/profile-detail';
 import Contact from './views/contact';
 import Cart from './views/cart';
+import Create from './views/create';
 import {MenuOptions} from './parts/menu';
+import CartDetails from './views/cart-details';
+import {useCookies} from "react-cookie";
 
 export const App = () => {
+    const [itemsCart, setItemsCart] = useState([]);
+    const [cartCookie, setCartCookie] = useCookies(['cart-items'])
+    const setNewItem = (item) => {
+        const exist = itemsCart.find((x) => x.id === item.id);
+        if (exist) {
+            setItemsCart(
+                itemsCart.map((x) =>
+                    x.id === item.id ? {...exist, qty: exist.qty + 1} : x)
+            );
+
+        } else {
+            setItemsCart([...itemsCart, {...item, qty: 1}]);
+        }
+        setCartCookie('cart-items', itemsCart, {sameSite: 'none', secure: true, path: '/'})
+    }
     return (
         <div className="App">
-            <header className="App-header mayus">
-                <h3>TejeRepuestos</h3>
+            <header className="App-header mayus bg-main">
+                <h3 className='bg-title-nav'>TejeRepuestos</h3>
             </header>
             <div className="d-flex space-b">
-                <div className='container-left bg-main'>
+                <div className='container-left'>
                     <MenuOptions />
                 </div>
                 <div className='container center'>
                     <Routes>
                         <Route path='/perfil' caseSensitive={false} element={<Auth />} />
-                        <Route path='/repuestos' caseSensitive={false} element={<Replacements />} />
+                        <Route path='/repuestos' caseSensitive={false} element={
+                            <Replacements itemsCart={itemsCart} setNewItem={setNewItem} />
+                        } />
                         <Route path='/contacto' caseSensitive={false} element={<Contact />} />
                         <Route path='/carro' caseSensitive={false} element={<Cart />} />
-                        <Route path='/perfil/detalles' caseSensitive={false} element={<ProfileDetails />} />
+                        <Route path='/crear' caseSensitive={false} element={<Create />} />
+                        <Route path='/perfil/detalles' caseSensitive={false}
+                            element={<ProfileDetails />} />
+                        <Route path='/cart/detalles' caseSensitive={false}
+                            element={
+                                <CartDetails cartCookie={cartCookie} />
+                            }
+                        />
                     </Routes>
                 </div>
-            </div>
-            <div className='App-footer'>
-                Copyright © 2022 TejeRepuestos
+            </div >
+            < div className='App-footer bg-main'>
+                <h6 className='bg-title-nav m-0'>Copyright © 2022 TejeRepuestos</h6>
             </div>
         </div>
     );
