f60c1ab960ab03304b0f95256bd9a086617f0f8e
diego <diegogomez268@gmail.com>
2022-06-18 09:17:43 -0500

    contact view some stetics change

--- src/views/contact.js
+++ src/views/contact.js
@@ -4,16 +4,17 @@ import {InputSign, TextareaSign, ButtonSign} from '../css/btn.js';
 function Contact() {
     return (
         <div className="mt-menu">
-            <h1>Contacta Con Nosotros</h1>
+            <h1>Hola</h1>
+            <h4 className="gray">Envianos un correo</h4>
             <form action="">
                 <div>
-                    <InputSign type="text" placeholder="Nombre" />
+                    <InputSign type="text" placeholder="Nombre" className='input-out' />
                 </div>
                 <div>
-                    <InputSign type="text" placeholder="Correo" />
+                    <InputSign type="text" placeholder="Correo" className='input-out' />
                 </div>
                 <div>
-                    <TextareaSign placeholder="Mensaje" />
+                    <TextareaSign placeholder="Mensaje" className='input-out' />
                 </div>
                 <ButtonSign>Enviar Mensaje</ButtonSign>
             </form>
