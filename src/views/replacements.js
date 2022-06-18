913b515e48da81a7cbc5106207099089737362b0
diego <diegogomez268@gmail.com>
2022-06-18 09:15:49 -0500

    replacements views main page

--- src/views/replacements.js
+++ src/views/replacements.js
@@ -2,18 +2,73 @@ import React, {useState, useEffect} from 'react';
 import HeadContent from '../parts/head-content';
 import PartList from '../parts/parts-list.js'
 import Sectors from '../parts/sector';
+import PartDetails from '../parts/part-details';
 import {Api} from './api-service';
 
-function Replacements() {
+function Replacements(props) {
+    const {itemsCart, setNewItem} = props;
+    const [machines, setMachines] = useState([]);
+    const [sectors, setSectors] = useState([]);
+    const [sector, setSector] = useState([]);
     const [parts, setParts] = useState([]);
+    const [part, setPart] = useState([]);
+    const [partDetail, setPartDetail] = useState(false);
+    const [activeTab, setActiveTab] = useState('esquema');
     useEffect(() => {
-        Api.getReplacementsList().then((resp => setParts(resp)))
+        Api.machineList().then(resp => setComponents(resp))
     }, [])
+
+    const setComponents = resp => {
+        setMachines(resp);
+        Api.sectorDetail(resp[0]['id']).then(resp => setSecondComponents(resp))
+    }
+
+    const setSecondComponents = resp => {
+        if (resp !== 'false') {
+            setSectors(resp)
+            setSector(resp[0])
+            Api.partDetail(resp[0]['id']).then(resp => setParts(resp))
+        } else {
+            setSectors(null)
+            setParts(null)
+        }
+    }
+
+    const clickedMachine = (itemM) => {
+        Api.sectorDetail(itemM['id']).then(resp => setSecondComponents(resp))
+    }
+
+    const clickedSector = sector => {
+        setSector(sector)
+        Api.partDetail(sector['id']).then(resp => resp === 'false' ?
+            setParts(null) : setParts(resp))
+    }
+    const clickedTab = tab => {
+        setActiveTab(tab)
+    }
+    const clickedPart = part_id => {
+        setPartDetail(true)
+        Api.partDetailPopup(part_id).then(resp => setPart(resp))
+    }
+    const closePopup = () => {
+        setPartDetail(false)
+    }
     return (
         <div>
-            <HeadContent></HeadContent>
-            <Sectors></Sectors>
-            <PartList parts={parts} />
+            {partDetail ?
+                <PartDetails part={part} closePopup={closePopup} itemsCart={itemsCart} setNewItem={setNewItem}></PartDetails> : null
+            }
+            <HeadContent machines={machines} clickedMachine={clickedMachine}></HeadContent>
+            <Sectors sectors={sectors}
+                clickedSector={clickedSector}
+                clickedTab={clickedTab}>
+            </Sectors>
+            {activeTab === 'esquema' ?
+                <div >
+                    <img src={sector.img} alt={sector.id} className="part-img" />
+                </div> :
+                <PartList parts={parts} clickedPart={clickedPart} />
+            }
         </div>
     )
 }
