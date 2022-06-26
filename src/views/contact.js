308b256849c2652989e2ca4c5783f7ccdeb2303c
diego <diegogomez268@gmail.com>
2022-06-26 11:01:22 -0500

    send msm contact function

--- src/views/contact.js
+++ src/views/contact.js
@@ -1,23 +1,70 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {InputSign, TextareaSign, ButtonSign} from '../css/btn.js';
+import {useCookies} from "react-cookie";
+import {Api} from './api-service';
 
 function Contact() {
+    const [token] = useCookies(['token']);
+    const [user, setUser] = useState('')
+    const [name, setInputName] = useState('')
+    const [email, setInputMail] = useState('')
+    const [msm, setInputMsm] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+    const [successMessage, setSuccessMessage] = useState('')
+
+    useEffect(() => {
+        if (token['token']) {
+            Api.get_user_id(token['token'])
+                .then((resp => Api.profile(token['token'], resp.user_id)
+                    .then((resp => setUser(resp)))))
+            setInputName(user['username'])
+            setInputMail(user['email'])
+        }
+    }, [token, user['username'], user['email']])
+
+    const setUsername = () => evt => {
+        setInputName(evt.target.value)
+    }
+    const setEmail = () => evt => {
+        setInputMail(evt.target.value)
+    }
+    const setMsm = () => evt => {
+        setInputMsm(evt.target.value)
+    }
+    const sendMsm = () => {
+        Api.sendMsm({username: name, email: email, msm: msm})
+            .then(resp =>
+                resp !== 'done' ? setErrorMessage(resp) : setSuccessMessage('Mensaje enviado'))
+    }
+
     return (
         <div className="mt-menu">
             <h1>Hola</h1>
-            <h4 className="gray">Envianos un correo</h4>
-            <form action="">
-                <div>
-                    <InputSign type="text" placeholder="Nombre" className='input-out' />
-                </div>
-                <div>
-                    <InputSign type="text" placeholder="Correo" className='input-out' />
-                </div>
-                <div>
-                    <TextareaSign placeholder="Mensaje" className='input-out' />
-                </div>
-                <ButtonSign>Enviar Mensaje</ButtonSign>
-            </form>
+            {errorMessage && <div className="alert-danger">{errorMessage}</div>}
+            {successMessage && <div className="alert-success">{successMessage}</div>}
+            <div>
+                <InputSign type="text"
+                    placeholder='Nombre'
+                    value={token ? user['username'] : null}
+                    onChange={setUsername()}
+                    className='input-out' />
+            </div>
+            <div>
+                <InputSign type="text"
+                    placeholder="Correo"
+                    value={token ? user['email'] : null}
+                    onChange={setEmail()}
+                    className='input-out' />
+            </div>
+            <div>
+                <TextareaSign
+                    placeholder="Mensaje"
+                    onChange={setMsm()}
+                    className='input-out' />
+            </div>
+            <ButtonSign onClick={() => sendMsm()}>
+                Enviar Mensaje
+            </ButtonSign>
         </div>
     )
 }
