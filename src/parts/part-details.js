6814c5215be6d235a92aa117540cd42007f08b46
diego <diegogomez268@gmail.com>
2022-07-04 11:24:43 -0500

    styling and distribution on part-details

--- src/parts/part-details.js
+++ src/parts/part-details.js
@@ -12,6 +12,8 @@ function PartDetails(props) {
     const [itemsCart, setItemsCart] = useState([props['itemsCart']]);
     const [addActive, setAddActive] = useState(false);
     const [itemCart, setItemCart] = useState([]);
+    const options = {style: 'currency', currency: 'MXN'};
+    const numberFormat = new Intl.NumberFormat('es-MX', options);
 
     useEffect(() => {
         setAddActive(false)
@@ -49,63 +51,66 @@ function PartDetails(props) {
     return (
         <div className='part-details'>
             <FontAwesomeIcon className='icon-popup' icon={faClose} onClick={() => closePopup()} />
-            <h1 className=' center w-50 mt-1'>{props.part.description}</h1>
             <div className='d-flex center space-a'>
-                <h2 className='mayus'>precio</h2>
-                <h2 >${props.part.price}</h2>
-            </div>
-            <div className='photo center'>
-                <img src={props.part.photo} alt="" />
-            </div>
-            <div className='mb-3'>
-                <h4 className='w-50 center'>Inventario: {props.part.stock} disponibles</h4>
-                <h5 className='purple'> Agrega al carrito de compra</h5>
-                <div>
-                    {props.part.stock === 0 || !token['token'] ?
-                        <div>
-                            <h5 className='w-50 center'>
-                                Para poder comprar regístrate o quizás no hay inventario
-                            </h5>
-                        </div>
-                        :
-                        <div>
-                            {props.part.stock === itemCart.qty ?
-                                <div>
-                                    <h3>No puedes agregar mas repuestos, se agoto el inventario</h3>
-                                    {!addActive || itemCart.qty < 1 ?
-                                        <ButtonAdd onClick={() => setNewItem(props.part)}>agregar</ButtonAdd> :
-                                        <div className='d-flex center space-b w-300 add-articles'>
-                                            <FontAwesomeIcon icon={faTrash} className='center white'
-                                                onClick={() => deleteItem(props.part)} />
-                                            <h4 className='white'>Cantidad: </h4>
-                                            <h4 className='white ml-1'>{!itemCart.qty ? 0 : itemCart.qty}</h4>
-                                            <FontAwesomeIcon icon={faSubtract}
-                                                className='center white'
-                                                onClick={() => removeItem(props.part)} />
-                                        </div>
-                                    }
-                                </div>
-                                :
-                                <div>
-                                    {!addActive || itemCart.qty < 1 ?
-                                        <ButtonAdd onClick={() => setNewItem(props.part)}>agregar</ButtonAdd> :
-                                        <div className='d-flex center space-b w-300 add-articles'>
-                                            <FontAwesomeIcon icon={faTrash} className='center white'
-                                                onClick={() => deleteItem(props.part)} />
-                                            <h4 className='white'>Cantidad: </h4>
-                                            <h4 className='white ml-1'>{!itemCart.qty ? 0 : itemCart.qty}</h4>
-                                            <FontAwesomeIcon icon={faSubtract}
-                                                className='center white'
-                                                onClick={() => removeItem(props.part)} />
-                                            <FontAwesomeIcon icon={faAdd}
-                                                onClick={() => setNewItem(props.part)}
-                                                className='center white' />
-                                        </div>
-                                    }
-                                </div>
-                            }
-                        </div>
-                    }
+                <div className='photo w-50'>
+                    <img src={props.part.photo} alt="" />
+                </div>
+                <div className='mb-3 w-50'>
+                    <h1 className='center mt-3'>{props.part.description}</h1>
+                    <h2 >{numberFormat.format(props.part.price)}</h2>
+                    <h4 className='gray'>Inventario: <strong className='purple'> {props.part.stock}</strong>  disponibles</h4>
+                    <h5 className='purple'> Agrega al carrito de compra</h5>
+                    <div>
+                        {props.part.stock === 0 || !token['token'] ?
+                            <div>
+                                <h5 className='w-50 center'>
+                                    Para poder comprar regístrate o quizás no hay inventario
+                                </h5>
+                            </div>
+                            :
+                            <div>
+                                {props.part.stock === itemCart.qty ?
+                                    <div>
+                                        <h3>No puedes agregar mas repuestos, se agoto el inventario</h3>
+                                        {!addActive || itemCart.qty < 1 ?
+                                            <ButtonAdd onClick={() => setNewItem(props.part)}>agregar</ButtonAdd> :
+                                            <div className='d-flex center space-b w-300 add-articles'>
+                                                <FontAwesomeIcon icon={faTrash} className='center white'
+                                                    onClick={() => deleteItem(props.part)} />
+                                                <h4 className='white'>Cantidad: </h4>
+                                                <h4 className='white ml-1'>{!itemCart.qty ? 0 : itemCart.qty}</h4>
+                                                <FontAwesomeIcon icon={faSubtract}
+                                                    className='center white'
+                                                    onClick={() => removeItem(props.part)} />
+                                            </div>
+                                        }
+                                    </div>
+                                    :
+                                    <div>
+                                        {!addActive || itemCart.qty < 1 ?
+                                            <ButtonAdd onClick={() => setNewItem(props.part)}>agregar</ButtonAdd> :
+                                            <div className='d-flex center space-b w-300 add-articles'>
+                                                <FontAwesomeIcon icon={faTrash} className='center white'
+                                                    onClick={() => deleteItem(props.part)} />
+                                                <h4 className='white'>Cantidad: </h4>
+                                                <h4 className='white ml-1'>{!itemCart.qty ? 0 : itemCart.qty}</h4>
+                                                <FontAwesomeIcon icon={faSubtract}
+                                                    className='center white'
+                                                    onClick={() => removeItem(props.part)} />
+                                                <FontAwesomeIcon icon={faAdd}
+                                                    onClick={() => setNewItem(props.part)}
+                                                    className='center white' />
+                                            </div>
+                                        }
+                                    </div>
+                                }
+                            </div>
+                        }
+                    </div>
+                    <h6 className='m-0 mt-1 gray mayus'>referencia</h6>
+                    <h6 className='m-0'> {props.part.reference}</h6>
+                    <h6 className='m-0 mt-1 gray mayus'>codigo</h6>
+                    <h6 className='m-0'>{props.part.code}</h6>
                 </div>
             </div>
         </div>
